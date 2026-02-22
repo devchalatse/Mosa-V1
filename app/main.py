a4ab953750cd7f8a05d0d205ad1720d94ac395bf
devchalatse <chalatsethabo@gmail.com>
2026-02-23 00:11:04 +0200

    changes in models

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
-from db.base import Base
-from db.session import engine
-from modules.users.router import router
+from app.db.base import Base
+# from modules.users.router import router
 # from modules.user.router import router
 # from modules.users.router import router
 
 app = FastAPI()
 
 # Create tables
-Base.metadata.create_all(bind=engine)
+
 
 # Include routers
-app.include_router(router)
+# app.include_router(router)
 
