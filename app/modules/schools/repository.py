d112f6b5e11c8f39d32b791c037d34773193f698
devchalatse <chalatsethabo@gmail.com>
2026-03-11 19:22:09 +0200

    added schools module

---
+++ app/modules/schools/repository.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from .models import Schools
+from .schemas import SchoolCreate
+
+class SchoolRepository:
+    def __init__(self, db: Session):  
+        self.db = db
+
+    def get_schools(self):
+        return self.db.query(Schools).all()  
+
+    def get_school_by_id(self, school_id: int):
+        return self.db.query(Schools).filter(Schools.id == school_id).first()
+
+    def get_school_by_email(self, email: str):
+        return self.db.query(Schools).filter(Schools.email == email).first()  
+
+    def create(self, data: SchoolCreate):
+        school = Schools(**data.model_dump())
+        self.db.add(school)        
+        self.db.commit()
+        self.db.refresh(school)   
+        return school
---
+++ app/modules/schools/service.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from .schemas import SchoolCreate
+from .repository import SchoolRepository
+
+class SchoolService:
+    def __init__(self, db: Session): 
+        self.repo = SchoolRepository(db)
+
+    def get_all_schools(self):
+        return self.repo.get_schools() 
+
+    def get_school_by_id(self, school_id: int):
+        school = self.repo.get_school_by_id(school_id)
+        if not school:
+            raise ValueError("School not found")
+        return school
+
+    def get_school_by_email(self, email: str):
+        school = self.repo.get_school_by_email(email)
+        if not school:
+            raise ValueError("School not found")
+        return school
+
+    def create(self, data: SchoolCreate):
+        return self.repo.create(data)
+
+    # def delete(self, school_id: int):
+    #     school = self.repo.get_school_by_id(school_id)
+    #     if not school:
+    #         raise ValueError("School not found")
+    #     return self.repo.delete(school_id)
