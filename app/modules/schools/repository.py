8f62ee3c67077031a8dc611680340777fa822ac6
devchalatse <chalatsethabo@gmail.com>
2026-03-14 02:56:44 +0200

    school service and repo

--- app/modules/schools/repository.py
+++ app/modules/schools/repository.py
@@ -2,32 +2,31 @@ from sqlalchemy.orm import Session
 from .models import Schools
 from .schemas import SchoolCreate
 
-class SchoolRepository:
-    def __init__(self, db: Session):  
+class RepositorySchools:
+    def __init__(self, db:Session):
         self.db = db
 
     def get_schools(self):
-        return self.db.query(Schools).all()  
-
-    def get_school_by_id(self, school_id: int):
-        return self.db.query(Schools).filter(Schools.id == school_id).first()
-
-    def get_school_by_email(self, email: str):
-        return self.db.query(Schools).filter(Schools.email == email).first()  
-
-    def create(self, data: SchoolCreate):
-        school = Schools(**data.model_dump())
-        self.db.add(school)        
-        self.db.commit()
-        self.db.refresh(school)   
-        return school
+        self.db.query(Schools).all
     
-    def delete(self, school_id:int):
-        school = self.db.query(Schools).filter(Schools.id == school_id).first()
-        if not school:
+    def get_school_id(self, school_id:int):
+        self.db.query(Schools).filter(Schools.id == school_id).first()
+    
+    def get_email_id(self, email_id:str):
+        self.db.query(Schools).filter(Schools.email == email_id).first()
+
+    def create_school(self, data:SchoolCreate):
+        school_data = Schools(**data.model_dump())
+        self.db.add(school_data)
+        self.db.commit()
+        self.db.refresh(school_data)
+        return school_data
+    
+    def delete(self, school_delete:int):
+        delete_school = self.db.query(Schools).filter(Schools.id == school_delete).first()
+        if not delete_school:
             return None
-            self.db.delete(school)
-            self.db.commit()
-            return school
-        
-        
+        self.db.commit()
+        self.db.delete(delete_school)
+
+        return delete_school
--- app/modules/schools/service.py
+++ app/modules/schools/service.py
@@ -1,31 +1,31 @@
 from sqlalchemy.orm import Session
-from .schemas import SchoolCreate
-from .repository import SchoolRepository
+from .schemas import SchoolCreate  
+from .repository import RepositorySchools
 
-class SchoolService:
-    def __init__(self, db: Session): 
-        self.repo = SchoolRepository(db)
+class ServiceSchools:
+    def __init__(self, db: Session):
+        self.repo = RepositorySchools(db)
 
     def get_all_schools(self):
-        return self.repo.get_schools() 
+        return self.repo.get_schools()
 
     def get_school_by_id(self, school_id: int):
-        school = self.repo.get_school_by_id(school_id)
+        school = self.repo.get_school_id(school_id)
         if not school:
             raise ValueError("School not found")
-        return school
+        return school  
 
-    def get_school_by_email(self, email: str):
-        school = self.repo.get_school_by_email(email)
-        if not school:
-            raise ValueError("School not found")
-        return school
+    def get_schools_email(self, email_id: str):
+        email = self.repo.get_email_id(email_id)
+        if not email:
+            raise ValueError("Email not found")
+        return email
 
     def create(self, data: SchoolCreate):
-        return self.repo.create(data)
+        return self.repo.create_school(data)
 
     def delete(self, school_id: int):
-        school = self.repo.get_school_by_id(school_id)
-        if not school:
-            raise ValueError("School not found")
-        return self.repo.delete(school_id) 
+        delete_id = self.repo.get_school_id(school_id)
+        if not delete_id:
+            raise ValueError("Invalid school details")
+        return self.repo.delete(school_id)
