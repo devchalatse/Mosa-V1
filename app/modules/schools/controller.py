6f7c183b008d9ac0ded4de18082e6cf20aa934a5
devchalatse <chalatsethabo@gmail.com>
2026-03-14 17:58:46 +0200

    schools controller added

---
+++ app/modules/schools/controller.py
@@ -0,0 +1,35 @@
+from fastapi import HTTPException
+from .service import ServiceSchools
+from .schemas import SchoolResponse, SchoolCreate
+from sqlalchemy.orm import Session
+
+class schoolController:
+    def __init__(self, db:Session):
+        self.service = ServiceSchools(db)
+
+    def get_all_schools(self):
+        schools = self.service.get_all_schools()
+        return {"status":"success", "data":schools}
+    
+    def get_schools_id(self, school_id: int):
+        try:
+            school = self.service.get_school_by_id(school_id)
+            return {"status":"success", "data": school}
+        except ValueError as e:
+            raise HTTPException(status_code=401,detail=str(e))
+        
+    def get_email_id(self, email_id:str):
+        try:
+            email = self.service.get_schools_email(email_id)
+            return {"status":"success", "data":email}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
+    
+    def create(self, data:SchoolCreate):
+        try:
+            create_data = self.service.create(data)
+            return{"status":"success", "data": create_data}
+        except ValueError as e:
+            raise HTTPException(status_code=400, detail=str(e))
+        
+    
