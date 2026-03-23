abf58ce483afff62fff2265e5ba2772d10ecff53
devchalatse <chalatsethabo@gmail.com>
2026-03-23 21:51:11 +0200

    changes in schema.py

--- app/modules/donations/schemas.py
+++ app/modules/donations/schemas.py
@@ -1,14 +1,12 @@
 from pydantic import BaseModel
 from .models import DonationStatus  
 
-# What frontend sends when creating a donation
 class DonationCreate(BaseModel):
     user_id: int
     school_id: int
     item_id: int
     quantity: int
 
-# What we send back to frontend
 class DonationResponse(BaseModel):
     id: int
     user_id: int
@@ -16,10 +14,8 @@ class DonationResponse(BaseModel):
     item_id: int
     quantity: int
     status: DonationStatus
-
     class Config:
         from_attributes = True
 
-# For updating donation status
 class DonationStatusUpdate(BaseModel):
     status: DonationStatus
