4695224fb7981bf04165fd55dd47bfa4ecd046d5
devchalatse <chalatsethabo@gmail.com>
2026-04-22 01:23:59 +0200

    changes in the db

--- app/modules/dashboard/controller.py
+++ app/modules/dashboard/controller.py
@@ -1,25 +1,53 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 from .model import dashboard
 from .schema import createDashboard
 from .service import dashboardService
 
-class DonationController:
+class dashboardController:
     def __init__(self, db:Session):
         self.service = dashboardService(db)
 
-    def get_all_users(self):
-        return self.service.get_all_dashboard()
-
-    def get_dashboard_by_id(self, dashboard_id:int):
-        return self.service.get_dashboard_by_id(dashboard_id)
+    def get_all_dashboard(self):
+        dashboard = self.service.get_all_dashboard()
+        return {"status":"success", "data": dashboard}
     
-    def get_dashboard_by_item(self, item_id:int):
-        return self.service.get_dashboard_by_item(item_id)
+    def get_dashboard_by_userid(self, user_id:int):
+        try:
+            user = self.service.get_dashboard_by_users(user_id)
+            return {"status":"success", "data": user_id}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
+        
+    def get_dashboard_by_items(self, items_id:int):
+        try:
+            item = self.service.get_dashboard_by_item(items_id)
+            return {"status":"success", "data": items_id}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
     
     def get_dashboard_by_school(self, school_id:int):
-        return self.service.get_dashboard_by_item(school_id)
+        try:
+            school = self.service.get_dashboard_by_school(school_id)
+            return {"status":"success", "data":school}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
     
-    
-    
+    def get_dashboard_donation(self, donation_id:int):
+        try:
+            donation = self.service.get_dashboard_by_donation(donation_id)
+            return {"status":"success", "data":donation}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
+        
+    def create_dashboard(self, data:createDashboard):
+        try:
+            create_dashboard = self.service.create_dashboard(data)
+            return {"status":"success", "data":data}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
+
     
         
+    
+    
--- app/modules/dashboard/service.py
+++ app/modules/dashboard/service.py
@@ -13,25 +13,37 @@ class dashboardService:
     def get_dashboard_by_id(self, dashboard_id:int):
         dashboard = self.get_dashboard_by_id(dashboard_id)
         if not dashboard:
-            raise ValueError("Dashboard not found")
+            raise ValueError("Dashboard not found in dashboard items")
         return dashboard
     
     def get_dashboard_by_school(self, school_id:int):
         School = self.repo.get_dashboard_school(school_id)
         if not School:
-            raise ValueError ("School not found")
+            raise ValueError ("School not found in dashboard items")
         return School
 
     def get_dashboard_by_item(self, item_id:int):
         Items = self.repo.get_dashboard_by_items(item_id)
         if not Items:
-            raise ValueError ("Items not found")
+            raise ValueError ("Items not found in dashboard items")
         return Items
 
     def get_dashboard_by_donation(self, donation_id:int):
         donation = self.repo.get_dashboard_by_donation(donation_id)
         if not donation:
-            raise ValueError ("donation nof found")
+            raise ValueError ("donation nof found in dashboard items")
         return donation
     
+    def get_dashboard_by_users(self, user_id:int):
+        user = self.repo.get_dashboard_by_users(user_id)
+        if not user:
+            raise ValueError ("users not found in dashboard items")
+        return user
+    
+    def create_dashboard(self, data: createDashboard):
+        return self.repo.create_dashboard(data)
+    
+    
+    
+
     
