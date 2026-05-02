f1efa7181b4603fb1ad11fe587a1c330b73cd433
devchalatse <chalatsethabo@gmail.com>
2026-05-02 19:53:25 +0200

    changes in the repo

--- app/modules/dashboard/repository.py
+++ app/modules/dashboard/repository.py
@@ -7,7 +7,37 @@ class dashboardRepository:
     def __init__(self, db:Session):
         self.db = db
 
-    def get_dashboard_by_id(self):
+    def get_all_user_dashboard(self):
         return self.db.query(Dashboard).all()
     
+    def get_dashboard_by_id(self, dashboard_id:int):
+        return self.db.query(Dashboard).filter(Dashboard.id == dashboard_id).first()
+    
+    def get_dashboard_by_users(self, user_id:int):
+        return self.db.query(Dashboard).filter(Dashboard.user_id == user_id).all()
+    
+    def get_dashboard_by_items(self, items_id:int):
+        return self.db.query(Dashboard).filter(Dashboard.items_id == items_id).all()
+    
+    def get_dashboard_by_donation(self, donation_id:int):
+        return self.db.query(Dashboard).filter(Dashboard.donation_id == donation_id).all()
+    
+    def get_dashboard_school(self, school_id:int):
+        return self.db.query(Dashboard).filter(Dashboard.school_id == school_id).all()
+    
+    def create_dashboard(self, data:createDashboard):
+        Dashboard = Dashboard(**data.model_dump())
+        self.db.add(Dashboard)
+        self.db.commit()
+        self.db.refresh(Dashboard)
+        return Dashboard
+    
+    def delete(self, dashboard_id:int):
+        Dashboard = self.db.query(Dashboard).filter(Dashboard.donation_id == dashboard_id).first()
+        if not Dashboard:
+            return None
+        self.db.delete(Dashboard)
+        self.db.commit()
+        self.db.refresh(Dashboard)
+        return Dashboard
     
