13e8b04fa6a6817ad28df4f12831c92ddd96399e
devchalatse <chalatsethabo@gmail.com>
2026-08-21 09:23:03 +0200

    changes in the CI

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy import text
 
 from app.db.database import engine, Base
+# from app.db.database import engine, Base
 # Import models so SQLAlchemy registers them
 
 from app.modules.users import models
