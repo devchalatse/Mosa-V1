93844419e40414e41f63fd2a2477fa87bfd3e4de
devchalatse <chalatsethabo@gmail.com>
2026-09-16 23:05:17 +0200

    Done all the changes

--- app/modules/notifications/controller.py
+++ app/modules/notifications/controller.py
@@ -2,3 +2,21 @@ from .models import Notification
 from .schema import NotificationCreate
 from .service import NotificationService
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+class controllerNotification:
+    def __init__(self, db:Session):
+        self.service = NotificationService(db)
+
+    def get_all_notififactions(self):
+        not1 = self.service.get_all_notifications()
+        return{"status":"success", "data": not1}
+
+    def get_notification_by_id(self, notification_id:int):
+        try:
+            not2 = self.service.get_notification_by_id(notification_id)
+            return {"status":"success", "data": not2}
+        except ValueError as e:
+            raise HTTPException(status_code=401, detail=str(e))
+
+    
